ac2ac185fd00a548ca81cef6882488a46161d574
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-15 19:10:56 +0800

    完成获取一条热搜需求

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,28 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.RsEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<RsEvent> rsList = getRsList();
+
+    List<RsEvent> getRsList() {
+        ArrayList<RsEvent> rsEvents = new ArrayList<>();
+        rsEvents.add(new RsEvent("第一条事件", "无分类"));
+        rsEvents.add(new RsEvent("第二条事件", "无分类"));
+        rsEvents.add(new RsEvent("第三条事件", "无分类"));
+        return rsEvents;
+    }
+
+    @GetMapping("/rs/event/{index}")
+    public RsEvent getRsEvent(@PathVariable int index) {
+        return rsList.get(index-1);
+    }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,29 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    void getRsTest() throws Exception {
+//    {"eventName":"第一条事件","keyWord":"无分类"}
+        mockMvc.perform(get("/rs/event/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$.eventName").value("第一条事件"));
+    }
 }
