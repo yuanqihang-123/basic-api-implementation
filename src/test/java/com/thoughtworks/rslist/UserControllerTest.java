cd4f7f558e9ce8d1d0d0a08830f17887220982f5
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-16 21:47:17 +0800

    pass add_rs_event_and_photo_is_valid_test

--- src/test/java/com/thoughtworks/rslist/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/UserControllerTest.java
@@ -197,5 +197,18 @@ public class UserControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+@Test
+    void add_rs_event_and_photo_is_valid_test() throws Exception {
+        User user = new User("zhangsan","male",30,"zs@tw.com","11234567890");
+        RsEvent rsEvent = new RsEvent("猪肉涨价了", "经济",user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rsEvent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
 
 }
