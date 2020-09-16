dc2ab51991ce3d54f4100d40024d8bb74bd3c0a0
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-16 21:31:19 +0800

    pass add_rs_event_and_gender_is_valid_test

--- src/test/java/com/thoughtworks/rslist/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/UserControllerTest.java
@@ -80,5 +80,18 @@ public class UserControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+@Test
+    void add_rs_event_and_gender_is_valid_test() throws Exception {
+        User user = new User("zhangsan","male",20,"zs@tw.com","11234567890");
+        RsEvent rsEvent = new RsEvent("猪肉涨价了", "经济",user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rsEvent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
 
 }
