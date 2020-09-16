5c8f30e2aa9533903c21ddcbc996e3837c4a7a46
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-16 21:27:06 +0800

    pass add_rs_event_and_userName_not_empty_test

--- src/test/java/com/thoughtworks/rslist/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/UserControllerTest.java
@@ -39,5 +39,16 @@ public class UserControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    void add_rs_event_and_userName_not_more_than_8_test() throws Exception {
+        User user = new User("123456789","male",20,"zs@tw.com","11234567890");
+        RsEvent rsEvent = new RsEvent("猪肉涨价了", "经济",user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(rsEvent);
 
+        mockMvc.perform(post("/rsEvent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
 }
